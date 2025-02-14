849f6633d41907bb89e9bdce3ec0dbb011088967
zaron4k <76218026+zaron4k@users.noreply.github.com>
2025-02-14 12:19:35 +0200

    1

---
+++ ConsoleApp5/Calculate.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{/// <summary>
+/// класс Calculate
+/// </summary>
+    class Calculate
+    {
+  
+     
+            private double field_1;
+            private double field_2;
+
+            public double Field_1 { get => field_1; set => field_1 = value; }
+            public double Field_2 { get => field_2; set => field_2 = value; }
+        
+            /// <summary>
+            /// Конструктор по умолчанию
+            /// </summary>
+            public Calculate() { }
+
+            /// <summary>
+            /// Конструктор с 1 параметром
+            /// </summary>
+            /// <param name="field_1">Поле 1</param>
+            public Calculate(double field_1)
+            {
+                this.Field_1 = field_1;
+            }
+
+            /// <summary>
+            /// Конструктор с 2 параметрами
+            /// </summary>
+            /// <param name="field_1">Поле 1</param>
+            /// <param name="field_2">Поле 2</param>
+            public Calculate(double field_1, double field_2)
+            {
+                this.Field_1 = field_1;
+                this.Field_2 = field_2;
+            }
+
+            /// <summary>
+            /// Метод для вывода значений полей.
+            /// </summary>
+            public void Output()
+            {
+                Console.WriteLine($"field1={Field_1}, field2={Field_2}");
+            }
+
+            /// <summary>
+            /// Метод для вычисления квадратного корня из произведения полей
+            /// </summary>
+            public double CalculateSquareRoot()
+            {
+                if (Field_1 < 0 || Field_2 < 0)
+                {
+                    throw new ArgumentException("Значения полей не могут быть отрицательными для вычисления квадратного корня.");
+                }
+                return Math.Sqrt(Field_1 * Field_2);
+            }
+
+            /// <summary>
+            /// Метод для преобразования объекта в строку
+            /// </summary>
+            public override string ToString()
+            {
+                return $"Calculate: Field_1 = {Field_1}, Field_2 = {Field_2}";
+            }
+
+            /// <summary>
+            /// Метод, который необходимо реализовать
+            /// </summary>
+            internal void Func()
+            {
+                // Реализовать функциональность метода здесь.
+                throw new NotImplementedException("Этот метод пока не реализован.");
+            }
+        }
+
+    }
+
+
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание объекта без параметров
+            Calculate calc = new Calculate();
+            calc.Output();
+            Console.WriteLine(calc.ToString());
+
+            // Изменение значений полей
+            calc.Field_1 = 3;
+            calc.Field_2 = -10;
+            Console.WriteLine(calc.ToString());
+
+            // Создание объекта с одним параметром
+            Calculate calc_1 = new Calculate(4);
+            calc_1.Output();
+
+            // Создание объекта с двумя параметрами
+            Calculate calc_2 = new Calculate(2, 3);
+            calc_2.Output();
+            calc_2.Func();
+
+            // Создание объекта с parsed значением (передаем значение для field_1 и Field_2)
+            string input = "5";  // Пример входных данных
+            double field_1 = double.Parse(input);
+            Calculate c = new Calculate(field_1, field_1);
+
+            Console.Read();
+        }
+        }
+    }
